953e9e953f50d1d771e348bbd333e8ac65e7a67a
jaseembinmuhammed@gmail.com <jaseembinmuhammed@gmail.com>
2025-03-11 00:05:26 +0530

    initial commit

---
+++ src/main/java/com/service/perkpoint/PerkpointApplication.java
@@ -0,0 +1,13 @@
+package com.service.perkpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerkpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerkpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/perkpoint/PerkpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.perkpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerkpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
